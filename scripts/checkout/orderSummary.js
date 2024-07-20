6661e0b64b0d0a713cdf5301694299e703a4467b
Mohamed Naeem <naemm2048@gmail.com>
2024-07-20 19:57:58 +0300

    Make paymentSummary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,14 +3,9 @@ import {products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';   
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
-
-
     let cartSummaryHTML = '';
 
     cart.forEach((cartItem) => {
@@ -112,6 +107,8 @@ export function renderOrderSummary() {
             const container = document.querySelector(`.js-cart-item-container-${productId}`
             );
             container.remove();
+
+            renderPaymentSummary();
         })
     });
 
@@ -123,6 +120,7 @@ export function renderOrderSummary() {
 
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 }
