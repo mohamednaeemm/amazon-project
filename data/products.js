6e3f7e025ee5c4d4e97eae68f0c7486a27db4e74
Mohamed Naeem <naemm2048@gmail.com>
2024-07-22 17:24:40 +0300

    Load products from backend

--- data/products.js
+++ data/products.js
@@ -52,8 +52,31 @@ class Clothing extends Product{
         return `
         <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>`
     }
-}
+} 
 
+export let products = [];
+
+export function loadProducts(fun) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', () => {
+        products = JSON.parse(xhr.response).map((productDetails) => {
+            if (productDetails.type === 'clothing') {
+                return new Clothing(productDetails);
+            }
+            return new Product(productDetails);
+        });
+
+        console.log('load products');
+
+        fun();
+    });
+
+    xhr.open('GET', 'https://supersimplebackend.dev/products');
+    xhr.send();
+};
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -721,3 +744,4 @@ export const products = [
 });
 
 
+*/
